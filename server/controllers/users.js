738c138ebeaad34bfb1f6759be5871e647f23758
emresen20 <emrebernarn@gmail.com>
2024-12-23 22:36:16 +0300

    signup

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -24,7 +24,7 @@ const signin=async (req,res)=>{
             )
         }
 
-        const token = jwt.sign({email:'user.email',id:'user._id'},'emre-secret-key',{expiresIn:'3h'})
+        const token = jwt.sign({email:user.email, id:user._id},'emre-secret-key',{expiresIn:'3h'})
 
         res.status(200).json({result:user,token})
 
@@ -36,7 +36,29 @@ const signin=async (req,res)=>{
 }
 
 const signup=async (req,res)=>{
-    res.send('signup')
+    const {email,
+        password,
+        confirmPassword,
+        firstName,
+        lastName
+    }= req.body;
+
+    try {
+        const user =await User.findOne({email})
+
+        if(user) return res.status(400).json({message:'Kullanıcı Zaten Bulunuyor '})
+        
+        if(password !== confirmPassword) return res.status(400).json({message:'Parolalar uyuşmadı'})
+
+         const hashedPassword=await bcrypt.hash(password,12)   
+        const result=await User.create({email,password:hashedPassword,name:`${firstName} ${lastName}`});
+        const token = jwt.sign({email:result.email,id:result._id},'emre-secret-key',{expiresIn:'3h'})
+        res.status(200).json({result,token})
+
+    } catch (error) {
+        res.status(500).json({message:'Bir Hata oluştu'})
+    }
+
 
 }
 
