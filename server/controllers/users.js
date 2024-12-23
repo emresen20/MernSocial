de7b6da4444e9a6486c5f58622e08aa753071b66
emresen20 <emrebernarn@gmail.com>
2024-12-23 22:05:22 +0300

    sigin controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,37 @@
 
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+import User  from '../models/user.js'
+
 
 const signin=async (req,res)=>{
-    res.send('signin')
+    const {email,password}=req.body;
+     
+    try {
+        const user=await User.findOne({email})
+
+        if(!user) {
+            return(
+                res.status(404).json({message:'Kullanıcı Bulunamadı'})
+            ) 
+        }
+
+        const passwordControlResult=await bcrypt.compare(password,user.password)
+        if(!passwordControlResult){
+            return(
+                res.status(400).json({message:'Parolayı Doğru giriniz'})
+            )
+        }
+
+        const token = jwt.sign({email:'user.email',id:'user._id'},'emre-secret-key',{expiresIn:'3h'})
+
+        res.status(200).json({result:user,token})
+
+    } catch (error) {
+        res.status(500).json({message:'Bir Hata oluştu daha sonra tekrar deneyiniz'})
+        
+    }
 
 }
 
