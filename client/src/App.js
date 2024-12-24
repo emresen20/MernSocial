707c84d04ea182b22d7ce4af70e343751f3c016d
emresen20 <emrebernarn@gmail.com>
2024-12-24 22:24:00 +0300

    signin component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Container } from "@mui/material";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/signin" element={<Signin/>}/>
         </Routes>
       </Container>
     </BrowserRouter>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,38 @@
 import React from 'react'
-import {AppBar,Typography} from '@mui/material'
+import {AppBar,Button,Toolbar,Typography} from '@mui/material'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <AppBar sx={{marginBottom:'15px',backgroundColor:'#edede9',position:'static',color:'#000',borderRadius:'10px'}}>
-        <Typography variant='h2' align='center'>Shen Social</Typography>
+    <AppBar sx={{
+    margin:'30px 0',
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    padding:'10px 50px',
+    backgroundColor:'#edede9',
+    position:'static',
+    color:'#000',
+    borderRadius:'10px',
+      
+    }}>
+       <div 
+        style={{
+          display:'flex',
+          alignItems:'center'
+        }}>
+        <Typography sx={{color:'black',textDecoration:'none'}} variant='h3' component={Link} to="/" align='center'>Shen Social</Typography>
+       </div>
+       <Toolbar sx={{
+        display:'flex',
+        justifyContent:'flex-end',
+        
+       }}>
+        <Button component={Link} to='/signin' variant='contained' color='secondary'>
+          Giriş Yap
+        </Button>
+       </Toolbar>
     </AppBar>
   )
 }
