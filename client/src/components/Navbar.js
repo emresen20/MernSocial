5808aa87844fd2061142f7c3678d246423d409fb
emresen20 <emrebernarn@gmail.com>
2024-12-28 23:03:04 +0300

    navbar for every special user

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import {AppBar,Button,Toolbar,Typography} from '@mui/material'
-import {Link} from 'react-router-dom'
+import React, {  useEffect, useState } from 'react'
+import {AppBar,Button,Toolbar,Typography,Avatar} from '@mui/material'
+import {Link,useLocation} from 'react-router-dom'
 
 function Navbar() {
+  const [user,setUser]=useState(JSON.parse(localStorage.getItem('profile')))
+  const location=useLocation();//uygulamada user değ,işikliğinde anlık olarak veri güncellemesinde kullanılır
+
+  useEffect(()=>{
+    setUser(JSON.parse(localStorage.getItem('profile')))
+  },[location])
+
   return (
     <AppBar sx={{
     margin:'30px 0',
@@ -29,9 +36,18 @@ function Navbar() {
         justifyContent:'flex-end',
         
        }}>
-        <Button component={Link} to='/signin' variant='contained' color='secondary'>
-          Giriş Yap
-        </Button>
+        {user?.result ?(
+          <div style={{display:'flex',width:'200px',alignItems:'center',gap:30}}>
+            <Avatar >
+              {user?.result.name.charAt(0)}
+            </Avatar>
+            <Button variant='contained' color='secondary'>Çıkış Yap</Button>
+          </div>
+        ):(
+           <Button component={Link} to='/signin' variant='contained' color='secondary'>
+           Giriş Yap
+         </Button>
+        )}
        </Toolbar>
     </AppBar>
   )
