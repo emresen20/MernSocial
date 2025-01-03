b0fde8ee1258c76cff66eaae32c39fb68d94da94
emresen20 <emrebernarn@gmail.com>
2025-01-04 00:42:50 +0300

    recommeded posts

--- client/src/components/PostDetail.js
+++ client/src/components/PostDetail.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import moment from "moment";
 import { getPost, getsPostsByTags } from "../actions/posts";
 import { CircularProgress, Paper, Typography, Divider } from "@mui/material";
@@ -9,20 +9,26 @@ export default function PostDetail() {
   const { post, posts, isLoading } = useSelector((state) => state.posts);
   const dispatch = useDispatch();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getPost(id));
   }, [id]);
 
   useEffect(() => {
-    if (post) {
-      dispatch(getsPostsByTags(post?.tags.join(",")));
+    if (post?.tags?.length > 0) {
+      dispatch(getsPostsByTags(post.tags.join(",")));
     }
   }, [post]);
+  
   //console.log('posts',posts)
 
-  const recommendPosts=posts.filter(({_id})=>_id!==post._id)//gelen datada bizim içinde olduğumuz postta olduğundan filteledik
-  console.log('recommendPosts',recommendPosts)
+  const recommendPosts = posts.filter(({ _id }) => _id !== post._id); //gelen datada bizim içinde olduğumuz postta olduğundan filteledik
+  //console.log('recommendPosts',recommendPosts)
+
+  const openPost = (_id) => {
+    navigate(`../posts/${_id}`);
+  };
 
   if (!post) return null;
 
@@ -70,11 +76,46 @@ export default function PostDetail() {
               width: "100%",
               height: "200px", // Resmin yüksekliği ayarlanabilir
             }}
-            src={post.selectedFile}
-            alt={post.title}
+            src={post.selectedFile || "placeholder-image-url"}
+            alt={post.title || "Post Image"}
           />
         </div>
       </div>
+      {!!recommendPosts.length && ( //Eğer recommendPosts.length sıfırsa (yani dizi boşsa), !! bunu false'a dönüştürür.
+        <div style={{ borderRadius: "20px", margin: "10ps", flex: 1 }}>
+          <Typography gutterBottom variant="h5">
+            Bu Postlar da ilginizi çekebilir
+          </Typography>
+          <Divider />
+          <div
+            style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+          >
+            {recommendPosts.map(
+              ({ title, name, message, likes, selectedFile, _id }) => (
+                <div
+                  style={{ margin: "20px", cursor: "pointer" }}
+                  key={_id}
+                  onClick={() => openPost(_id)}
+                >
+                  <Typography gutterBottom variant="h6">
+                    {title}
+                  </Typography>
+                  <Typography gutterBottom variant="subtitle2">
+                    {name}
+                  </Typography>
+                  <Typography gutterBottom variant="subtitle2">
+                    {message}
+                  </Typography>
+                  <Typography gutterBottom variant="subtitle1">
+                    Begeni Sayısı: {likes.length}
+                  </Typography>
+                  <img src={selectedFile} width="200px" />
+                </div>
+              )
+            )}
+          </div>
+        </div>
+      )}
     </Paper>
   );
 }
